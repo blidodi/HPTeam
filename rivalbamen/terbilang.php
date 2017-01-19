3fefbe46d628417325b91e5bc5622159af803e91
rivalbamen <rivalbamen@gmail.com>
2017-01-19 10:04:40 +0800

    Fungsi Piramida

--- rivalbamen/terbilang.php
+++ rivalbamen/terbilang.php
@@ -5,30 +5,30 @@
 <body>
 
 <?php
-function terbilang($x){
+function terbilang($an){
     $angka = array("", "Satu", "Dua", "Tiga", "Empat", "Lima",
     				"Enam", "Tujuh", "Delapan", "Sembilan", "Sepuluh", "Sebelas");
     $bilang = "";
-	    if ($x <12) {
-	        $bilang = " ".$angka[$x];
-	    } else if ($x <20) {
-	        $bilang = terbilang($x - 10)." Belas";
-	    } else if ($x <100) {
-	        $bilang = terbilang($x/10)." Puluh". terbilang($x % 10);
-	    } else if ($x <200) {
-	        $bilang = " Seratus".terbilang($x - 100);
-	    } else if ($x <1000) {
-	        $bilang = terbilang($x/100)." Ratus".terbilang($x % 100);
-	    } else if ($x <2000) {
-	        $bilang = " Seribu".terbilang($x - 1000);
-	    } else if ($x <1000000) {
-	        $bilang = terbilang($x/1000)." Ribu".terbilang($x % 1000);
-	    } else if ($x <1000000000) {
-	        $bilang = terbilang($x/1000000)." Juta".terbilang($x % 1000000);
-	    } else if ($x <1000000000000) {
-	        $bilang = terbilang($x/1000000000)." Milyar".terbilang(fmod($x,1000000000));
-	    } else if ($x <1000000000000000) {
-	        $bilang = terbilang($x/1000000000000)." Trilyun".terbilang(fmod($x,1000000000000));
+	    if ($an <12) {
+	        $bilang = " ".$angka[$an];
+	    } else if ($an <20) {
+	        $bilang = terbilang($an - 10)." Belas";
+	    } else if ($an <100) {
+	        $bilang = terbilang($an/10)." Puluh". terbilang($an % 10);
+	    } else if ($an <200) {
+	        $bilang = " Seratus".terbilang($an - 100);
+	    } else if ($an <1000) {
+	        $bilang = terbilang($an/100)." Ratus".terbilang($an % 100);
+	    } else if ($an <2000) {
+	        $bilang = " Seribu".terbilang($an - 1000);
+	    } else if ($an <1000000) {
+	        $bilang = terbilang($an/1000)." Ribu".terbilang($an % 1000);
+	    } else if ($an <1000000000) {
+	        $bilang = terbilang($an/1000000)." Juta".terbilang($an % 1000000);
+	    } else if ($an <1000000000000) {
+	        $bilang = terbilang($an/1000000000)." Milyar".terbilang(fmod($an,1000000000));
+	    } else if ($an <1000000000000000) {
+	        $bilang = terbilang($an/1000000000000)." Trilyun".terbilang(fmod($an,1000000000000));
 	    }     
 	        return $bilang;
 	}
