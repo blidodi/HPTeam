5db75bbfdbd1dba4266422a9ada7b95f7021d042
adhisurya <adhisuryanegara@gmail.com>
2017-01-19 10:37:10 +0800

    file

--- adhisurya/bilangan.php
+++ adhisurya/bilangan.php
@@ -1,28 +1,28 @@
 <?php
 
-  function bilangan($x) {
-      $angka = array("", "satu", "dua", "tiga", "empat", "lima","enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas");
+  function bilangan($nilai) {
+      $angka = array("", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas");
     $bilang = "";
-    if ($x <12) {
-        $bilang = " ". $angka[$x];
-    } else if ($x <20) {
-        $bilang = bilangan($x - 10). " belas";
-    } else if ($x <100) {
-        $bilang = bilangan($x/10)." puluh". bilangan($x % 10);
-    } else if ($x <200) {
-        $bilang = " seratus" . bilangan($x - 100);
-    } else if ($x <1000) {
-        $bilang = bilangan($x/100) . " ratus" . bilangan($x % 100);
-    } else if ($x <2000) {
-        $bilang = " seribu" . bilangan($x - 1000);
-    } else if ($x <1000000) {
-        $bilang = bilangan($x/1000) . " ribu" . bilangan($x % 1000);
-    } else if ($x <1000000000) {
-        $bilang = bilangan($x/1000000) . " juta" . bilangan($x % 1000000);
-    } else if ($x <1000000000000) {
-        $bilang = bilangan($x/1000000000) . " milyar" . bilangan(fmod($x,1000000000));
-    } else if ($x <1000000000000000) {
-        $bilang = bilangan($x/1000000000000) . " trilyun" . bilangan(fmod($x,1000000000000));
+    if ($nilai <12) {
+        $bilang = " ". $angka[$nilai];
+    } else if ($nilai <20) {
+        $bilang = bilangan($nilai - 10). " belas";
+    } else if ($nilai <100) {
+        $bilang = bilangan($nilai/10)." puluh". bilangan($nilai % 10);
+    } else if ($nilai <200) {
+        $bilang = " seratus" . bilangan($nilai - 100);
+    } else if ($nilai <1000) {
+        $bilang = bilangan($nilai/100) . " ratus" . bilangan($nilai % 100);
+    } else if ($nilai <2000) {
+        $bilang = " seribu" . bilangan($nilai - 1000);
+    } else if ($nilai <1000000) {
+        $bilang = bilangan($nilai/1000) . " ribu" . bilangan($nilai % 1000);
+    } else if ($nilai <1000000000) {
+        $bilang = bilangan($nilai/1000000) . " juta" . bilangan($nilai % 1000000);
+    } else if ($nilai <1000000000000) {
+        $bilang = bilangan($nilai/1000000000) . " milyar" . bilangan(fmod($nilai,1000000000));
+    } else if ($nilai <1000000000000000) {
+        $bilang = bilangan($nilai/1000000000000) . " trilyun" . bilangan(fmod($x,1000000000000));
     }     
       return $bilang;
   }
