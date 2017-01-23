ba89d15590f0b1aafbce388aa197edb1f83e6551
Bli Dodi <blidodi@gmail.com>
2017-01-23 12:53:51 +0800

    new file

---
+++ blidodi/belajar-array.php
@@ -0,0 +1,10 @@
+<?php
+
+$data = array(
+			array("Suzuki", "Blitz", 12000000),
+			array("Suzuki", "Ninja", 18000000),
+			array("Honda", "Vario", 16000000)
+	    );
+
+
+echo $data[2][1];
---
+++ chiput/formlogin.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Sign In</title>
+
+<link rel="stylesheet" type="text/css" href="styleForm.css" />
+
+</head>
+
+<body>
+
+<div id="carbonForm">
+    <h1><center>Sign In Your Account</center></h1>
+    <div class="fieldContainer">
+        
+        <div class="formRow">
+            <div class="label">
+                <label for="email"></label>
+            </div>
+            
+            <div class="field">
+                <input type="text" name="email" id="email" />
+            </div>
+        </div>
+        
+        <div class="formRow">
+            <div class="field">
+                <input type="password" name="pass" id="pass" />
+            </div>
+        </div>
+        
+        
+    </div>
+    
+    <div class="signinButton">
+        <input type="submit" name="submit" id="submit" value="Signin" />
+    </div>
+    </form>
+        
+</div>
+</body>
+</html>
