8fcb0ec572a8b227aff2ef73b468b299aeb5c92e
chiput <rizkyputri7@gmail.com>
2017-01-23 08:16:31 +0700

    Tugas Weekend 1 Form Login

--- chiput/formlogin.php
+++ chiput/formlogin.php
@@ -1,62 +1,44 @@
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Login</title>
-	<style type="text/css">
-		*{
-			width: 90%;
-			padding: 10px 20px;
-			border-radius: 5px;
-			color: gray;
-			background-color: #f1f1f1;
-		}
-		.table{
-			width: 70%;
-			background-color: #D3D3D3;
-			margin-top: :30%;
-			margin-bottom: 30%;
-			margin-left: 30%;
-			margin-right: 30%;
-		}
-		.table1{
-			width: 70%;
-			padding: 10px;
-			background-color: #D3D3D3;
-		}
-		.td{
-			background-color: #D3D3D3;
-		}
-		input[type=submit]{
-			width: 98%;
-			border-radius: 5px;
-			border-collapse: 5px;
-			padding: 10px 20px;
-			background-color: #20B2AA;
-		    color: white;
-		    cursor: pointer;
-		}
-	</style>
+
+<title>Sign In</title>
+
+<link rel="stylesheet" type="text/css" href="styleForm.css" />
+
 </head>
+
 <body>
-<center>
-<form action="tampil.php" method="get">
-<h1 style="text-align: center;">Log-In to Your Account</h1>
-<table class="table">
-	<tr class="table">
-		<td class="td"><input class="text" type="text" name="nomor" value="Email Address" /></td>
-	</tr>
-	<tr class="table">
-		<td class="td"><input class="text" type="password" name="nama" value="Password" /></td>
-	</tr>
-	<tr class="table">
-		<td class="td"><input type="submit" name="login" value="Login"/></td>
-	</tr>
-</table>
-<table class="table1">
-	<tr class="table1">
-	</tr>
-</table>
-</form>
-</center>
+
+<div id="carbonForm">
+    <h1><center>Sign In Your Account</center></h1>
+    <div class="fieldContainer">
+        
+        <div class="formRow">
+            <div class="label">
+                <label for="email"></label>
+            </div>
+            
+            <div class="field">
+                <input type="text" name="email" id="email" />
+            </div>
+        </div>
+        
+        <div class="formRow">
+            <div class="field">
+                <input type="password" name="pass" id="pass" />
+            </div>
+        </div>
+        
+        
+    </div>
+    
+    <div class="signinButton">
+        <input type="submit" name="submit" id="submit" value="Signin" />
+    </div>
+    </form>
+        
+</div>
 </body>
 </html>
