14fe406918a17a40478612fc5b9d3cb8ba89df4c
Kajhie <muhammadmirza25@gmail.com>
2017-01-25 15:47:28 +0800

    update

--- kajhie/tester/tabel.php
+++ kajhie/tester/tabel.php
@@ -28,18 +28,23 @@ mysql_select_db($base);
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
+<center>
+<h3> Table Gaji </h3>
 <table class="table" border="1">
 	<tr>
-		<td>No</td>
-		<td>Diterima Untuk</td>
-		<td>Nominal</td>
-		<td>Kebutuhan</td>
+		<th>No</th>
+		<th>Diterima Untuk</th>
+		<th>Nominal</th>
+		<th>Kebutuhan</th>
+		<th>Status</th>
+		<th>Operasi</th>
 	</tr>
 <?php
-	$sql = "SELECT * FROM `data`";
+//select `data`.*, `nama_status` as `status` from `data` join `status` where `data`.`no`=`status`.`id_status`
+// and  `status`.`nama_status`='Belum Lunas'
+	$sql = "SELECT `data`.*, `nama_status` as `status` from `data` join `status` where `data`.`no`=`status`.`id_status`";
 
 	$hasil = mysql_query($sql);
-
 	while ($data = mysql_fetch_array($hasil)) {
 ?>
 	<tr>
@@ -47,10 +52,15 @@ mysql_select_db($base);
 		<td><?php echo $data['dari']?></td>
 		<td><?php echo $data['nominal']?></td>
 		<td><?php echo $data['kebutuhan']?></td>
+		<td><?php echo $data['status']?></td>
+		<td>
+			<a class="button" href="">Update</a> <a class="button button1" href="proses_hapus.php?id=<?php echo $data['no']?>">Delete</a>
+		</td>
 	</tr>
 	<?php
 }
 	?>
 </table>
+</center>
 </body>
 </html>
