b2c6e3110d6a34a96f180d086832711850697a1d
rivalbamen <rivalbamen@gmail.com>
2017-01-26 15:17:22 +0800

    New Folder User

--- rivalbamen/database/hapus.php
+++ rivalbamen/database/hapus.php
@@ -1,19 +1,21 @@
 <?php
 
-if($_GET['id']){
+if(isset($_GET['id'])){
 	include "config.php";
 	$id = $_GET['id'];
-	$sql = "DELETE FROM `data` WHERE `data`.`id`=$id";
-	$sql1 = "DELETE FROM `status` WHERE `status`.`id_data`=$id";
+	$sql_data = "DELETE FROM `data` WHERE `data`.`id`=$id";
+	$sql_status = "DELETE FROM `status` WHERE `status`.`id_data`=$id";
 
-	$result = mysql_query($sql);
-	$result1 = mysql_query($sql1);
+	$result_data = mysql_query($sql_data);
+	$result_status = mysql_query($sql_status);
 
-	if($result && $result1) {
+	if($result_data && $result_status) {
 		header('location:index.php');
 	} else {
-		echo "Ada yang salah!!!";
+		echo "Ada Data yang salah!!!";
 	}
+} else {
+	echo "Anda dihalaman yang salah!!!";
 }
 
 ?>
---
+++ rivalbamen/user/add.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Tambah User</title>
+</head>
+<body>
+	<h1>Form User</h1><hr>
+	<p><a class="p-color" href="index.php">Tabel User</a> / Tambah User</p>
+	<form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+		<label>Nama</label>
+		<input type="text" name="nama" />
+		<label>Username</label>
+		<input type="text" name="username" />
+		<label>Password</label>
+		<input type="password" name="password" />
+		<label>Status</label>
+		<select name="role">
+			<option value="">--Pilih--</option>
+			<option value="1">Admin</option>
+			<option value="2">User</option>
+		</select>
+		<br/>
+		<input type="submit" name="simpan" value="Simpan">
+	</form>
+
+	<?php
+		if(isset($_POST['simpan']) && $_POST['simpan'] == 'Simpan') {
+			include "koneksi.php";
+
+			$sql_user = "INSERT INTO `user` (`nama`, `username`, `password`, `role`)
+								VALUES ('".$_POST['nama']."',
+										'".$_POST['username']."',
+										'".$_POST['password']."',
+										'".$_POST['role']."')";
+			$result = mysql_query($sql_user);
+			
+			/*$id = mysql_insert_id();
+			$sql_status = "INSERT INTO `status` (`id_data`, `nama`)
+								VALUES ('".$id."',
+										'".$_POST['status']."')";
+			$result = mysql_query($sql_status);
+			*/
+
+			if(isset($result)) {
+				header('location:index.php');
+			} else {
+				echo "Data Gagal Disimpan";
+			} 
+		}
+
+	?>
+
+</body>
+</html>
